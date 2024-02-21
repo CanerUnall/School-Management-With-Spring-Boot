eb7df1596c121f7b27fbd73d42ac469c27ca90ac
Caner Ünal <av.canerunal@gmail.com>
2024-02-21 23:09:10 +0100

    xx

---
+++ src/main/java/com/project/payload/messages/SuccessMessages.java
@@ -0,0 +1,10 @@
+package com.project.payload.messages;
+
+public class SuccessMessages {
+    private SuccessMessages() {
+    }
+
+    public static final String PASSWORD_CHANGED_RESPONSE_MESSAGE = "Password Changed Succesfully";
+    public static final String USER_CREATED = "User is Saved Successfully";
+    public static final String USER_FOUND = "User is Found Successfully";
+}
--- src/main/java/com/project/service/AuthenticationService.java
+++ src/main/java/com/project/service/AuthenticationService.java
@@ -1,7 +1,14 @@
 package com.project.service;
 
+import com.project.entity.concretes.user.User;
+import com.project.exception.BadRequestException;
+import com.project.payload.mappers.UserMapper;
+import com.project.payload.messages.ErrorMessages;
 import com.project.payload.request.authentication.LoginRequest;
+import com.project.payload.request.business.UpdatePasswordRequest;
+import com.project.payload.response.abstracts.BaseUserResponse;
 import com.project.payload.response.authentication.AuthResponse;
+import com.project.payload.response.user.UserResponse;
 import com.project.repository.user.UserRepository;
 import com.project.security.jwt.JwtUtils;
 import com.project.security.service.UserDetailsImpl;
@@ -12,8 +19,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -25,6 +34,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public ResponseEntity<AuthResponse> authenticateUser(LoginRequest loginRequest) {
         //!!! Gelen requestin icinden kullanici adi ve parola bilgisi aliniyor
@@ -32,7 +43,7 @@ public class AuthenticationService {
         String password = loginRequest.getPassword();
         // !!! authenticationManager uzerinden kullaniciyi valide ediyoruz
         Authentication authentication =
-                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         // !!! valide edilen kullanici Context e atiliyor
         SecurityContextHolder.getContext().setAuthentication(authentication);
         // !!! JWT token olusturuluyor
@@ -50,13 +61,37 @@ public class AuthenticationService {
         //!!! bir kullanicinin birden fazla rolu olmayacagi icin ilk indexli elemani aliyoruz
         Optional<String> role = roles.stream().findFirst();
 
-         AuthResponse.AuthResponseBuilder authResponse = AuthResponse.builder();
-         authResponse.username(userDetails.getUsername());
-         authResponse.token(token.substring(7));
-         authResponse.name(userDetails.getName());
-         authResponse.ssn(userDetails.getSsn());
-         role.ifPresent(authResponse::role);
+        AuthResponse.AuthResponseBuilder authResponse = AuthResponse.builder();
+        authResponse.username(userDetails.getUsername());
+        authResponse.token(token.substring(7));
+        authResponse.name(userDetails.getName());
+        authResponse.ssn(userDetails.getSsn());
+        role.ifPresent(authResponse::role);
 
-         return  ResponseEntity.ok(authResponse.build());
+        return ResponseEntity.ok(authResponse.build());
+    }
+
+    public UserResponse findByUsername(String username) {
+/*       User user = userRepository.findByUsernameEquals(username);
+       return userMapper.mapUserToUserResponse(user);*/
+        return userMapper.mapUserToUserResponse(userRepository.findByUsernameEquals(username));
+    }
+
+    public void updatePassword(UpdatePasswordRequest updatePasswordRequest, HttpServletRequest request) {
+
+        String userName = (String) request.getAttribute("username");
+        User user = userRepository.findByUsernameEquals(userName);
+        //!!! built_in kontrolu ??
+        if(Boolean.TRUE.equals(user.getBuilt_in())){
+            throw new BadRequestException(ErrorMessages.NOT_PERMITTED_METHOD_MESSAGE);
+        }
+        //!!! Eski sifre bilgisi dogru mu ??
+        if(!passwordEncoder.matches(updatePasswordRequest.getOldPassword(), user.getPassword())){
+            throw new BadRequestException(ErrorMessages.PASSWORD_NOT_MATCHED);
+        }
+        // !!! yeni sifre hashlenerek Kaydediliyor
+        String hashedPassword = passwordEncoder.encode(updatePasswordRequest.getNewPassword());
+        user.setPassword(hashedPassword);
+        userRepository.save(user);
     }
 }
